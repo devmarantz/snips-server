0d0d78850c5f30a6d2e3b64660944a02ef5cd92d
Devon Marantz <DMarantz@talentpath.com>
2019-07-25 08:59:57 -0700

    Define all routes

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('Welcome to Snips');
+});
+
+/* Snippets Route*/
+// POST /snippets
+app.post('/snippets', (req, res) => {});
+// GET /snippets
+app.get('/snippets', (req, res) => {});
+// GET /snippets/:id
+app.get('/snippets/:id', (req, res) => {});
+// PATCH /snippets/:id
+app.patch('/snippets/:id', (req, res) => {});
+// DELETE /snippets/:id
+app.delete('/snippets/:id', (req, res) => {});
+
 app.listen(5000, () => {
   console.log('Snips server running on Port 5000');
 });
