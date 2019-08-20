36ade1b80b3288d74549dd510a95b5660bbc2d87
Marantz <devonmarantz@gmail.com>
2019-08-20 10:49:03 -0700

    Add Cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const router = require('./middleware/routes');
 const logger = require('./middleware/logger');
 const errorHandler = require('./middleware/errorHandler');
@@ -6,6 +7,7 @@ const errorHandler = require('./middleware/errorHandler');
 const app = express();
 
 // Adds the router middleware
+app.use(cors());
 app.use(express.json()); //parse request with JSON bodies
 app.use(logger);
 app.use(router);
