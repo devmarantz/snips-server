8a5bb3f083053cde6f6271b88f133c4877bbaa4a
Devon Marantz <devonmarantz@gmail.com>
2019-09-24 15:02:37 -0700

    Add init.js

--- __tests__/integrations.test.js
+++ __tests__/integrations.test.js
@@ -1,7 +1,18 @@
 require('dotenv').config();
+const request = require('supertest');
+const app = require('../app');
 
 describe('Test', ()=> {
   it('should run a test', ()=>{
-    console.log(process.env.DATABASE_URL);
+    console.log(process.env.TEST_DATABASE_URL);
   });
+});
+
+describe('Snippets', ()=> {
+  describe('GET /api/snips', ()=> {
+    it('should get all of the snips', async ()=>{
+      const response = await request(app).get('/api/snippets');
+      console.log(response.body);
+    });
+  })
 });
